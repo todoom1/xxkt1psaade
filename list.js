af4520082b0c3e8f2f3efa70e87e642ca16ec011
todoom1 <aa13976383@outlook.com>
2026-03-26 20:48:07 +0800

    Update list.js

---
+++ list.js
@@ -0,0 +1,31 @@
+document.getElementById("jl").addEventListener("click", function() {
+	window.open(atob("11111"), '_blank');
+	});
+document.getElementById("cf").addEventListener("click", function() {
+	window.open(atob("11111"), '_blank');
+	});
+document.getElementById("ff").addEventListener("click", function() {
+	window.open(atob("11111"), '_blank');
+	});
+document.getElementById("dt").addEventListener("click", function() {
+	window.open(atob("11111"), '_blank');
+	});
+document.getElementById("sj").addEventListener("click", function() {
+	window.open(atob("11111"), '_blank');
+	});
+document.getElementById("xsd").addEventListener("click", function() {
+	window.open(atob("11111"), '_blank');
+	});
+document.getElementById("st").addEventListener("click", function() {
+	window.open(atob("11111"), '_blank');
+	});
+document.getElementById("jln").innerHTML = ("君临国际");
+document.getElementById("cfn").innerHTML = ("超凡国际");
+document.getElementById("ffn").innerHTML = ("非凡国际");
+document.getElementById("dtn").innerHTML = ("鼎天国际");
+document.getElementById("sjn").innerHTML = ("赏金国际");
+document.getElementById("xsdn").innerHTML = ("新时代国际");
+document.getElementById("stn").innerHTML = ("胜天国际");
+document.getElementById("2").innerHTML = ("🔥十年信誉<br>⚽世界杯官方投注平台");
+document.getElementById("3").innerHTML = ("2026世界杯");
+document.getElementById("1").innerHTML="66KT.MY";
