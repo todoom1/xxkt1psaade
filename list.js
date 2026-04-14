780987031fb0d1aef813b6eda018ad10ab369924
todoom1 <aa13976383@outlook.com>
2026-04-14 21:59:54 +0800

    Update list.js

--- list.js
+++ list.js
@@ -19,6 +19,10 @@ document.getElementById("xsd").addEventListener("click", function() {
 document.getElementById("st").addEventListener("click", function() {
 	window.open(atob("aHR0cHM6Ly9sZWp2YS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDU4JnRvcElkPTI2MjM4OTcmc2VsZlBsYW5JZD0yMTMxNzA1"), '_blank');
 	});
+window["document"]['getElementById']("hy")['addEventListener']("click", function () {
+    window['open'](atob("aHR0cHM6Ly9uY2djYS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDYyJnRvcElkPTEzNzMzJnNlbGZQbGFuSWQ9MTEzNw=="), '_blank')
+});
+window["document"]['getElementById']("hyn")['innerHTML'] = ("汇赢国际");
 document.getElementById("jln").innerHTML = ("君临国际");
 document.getElementById("cfn").innerHTML = ("超凡国际");
 document.getElementById("ffn").innerHTML = ("非凡国际");
@@ -29,4 +33,3 @@ document.getElementById("stn").innerHTML = ("胜天国际");
 document.getElementById("2").innerHTML = ("🔥十年信誉<br>⚽世界杯官方投注平台");
 document.getElementById("3").innerHTML = ("2026世界杯");
 document.getElementById("1").innerHTML="66KT.MY";
-//aHR0cHM6Ly9uY2djYS50cHJzbWkuY29tL2FwcC9yZWdpc3Rlci5waHA/c2l0ZV9pZD0xMDYyJnRvcElkPTEzNzMzJnNlbGZQbGFuSWQ9MTEzNw==
